1c5b00aa5fd4ca720f1a71359e143d474ca3619d
Georgios Papadopoulos <linkin2895@gmail.com>
2022-08-22 14:18:26 +0100

    lesson 4 implementation

--- src/exceptionExample/PhoneExceptionTester.java
+++ src/exceptionExample/PhoneExceptionTester.java
@@ -5,9 +5,9 @@ public class PhoneExceptionTester {
     public static void main(String[] args) {
         String[] numbers = new String[]{"123-4567", null, "234-4567", "345-5678"};
 
-        for (int i = 0; i < numbers.length; i++) {
+        for (String number : numbers) {
             try {
-                System.out.println(new Phone("iPhone", numbers[i]));
+                System.out.println(new Phone("iPhone", number));
             } catch (IllegalArgumentException ex) {
                 System.out.println(ex.getLocalizedMessage());
             }
